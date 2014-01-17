832e5c072ad3dfeb569b295526a27d63e1ebbeca
RC7502 <dev@bitsmack.com>
2014-01-16 22:53:27 -0500

    fixed unit test

--- BitsmackGTAPI.Tests/Controllers/PedometerServiceTest.cs
+++ BitsmackGTAPI.Tests/Controllers/PedometerServiceTest.cs
@@ -17,13 +17,14 @@ namespace BitsmackGTAPI.Tests.Controllers
     [TestClass]
     public class PedometerServiceTest
     {
-        private PedometerService _service; 
+        private readonly PedometerService _service; 
         private readonly Mock<IGTRepository<Pedometer>> _repo;
         private readonly Mock<ICommonService> _commonService;
 
         public PedometerServiceTest()
         {
             _repo = new Mock<IGTRepository<Pedometer>>();
+            _commonService = new Mock<ICommonService>();
             _service = new PedometerService(_repo.Object, _commonService.Object);
         }
 
